919625543ae1f473cdab42fd074237bb2888f1a8
Valencio-Saez <valenciosaez.ict@gmail.com>
2024-04-18 12:44:25 +0200

    Initial Commit

---
+++ Mind Eaze Watch App/Mind_EazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_EazeApp.swift
+//  Mind Eaze Watch App
+//
+//  Created by Valencio Saez ICT  on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mind_Eaze_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mind Eaze/Mind_EazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_EazeApp.swift
+//  Mind Eaze
+//
+//  Created by Valencio Saez ICT  on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mind_EazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
